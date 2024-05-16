525d25f8a1507e3820b6e1ce77baf07a86569df6
grotani <kimtery@naver.com>
2024-05-16 15:30:46 +0900

    [김태리, 박수지] 2024-05-16 commit2
    [추가]
    1. custoemrComplainOne.jsp 추가
      1.1  custoemrComplainList.jsp 에서 신고내역 상세보기 구현
      1.2 ComplainDAO 에 selectComplainOne 메서드 수정
    [수정]
    1. custoemrComplainList.jsp 수정
      1.1 답변보기 버튼 추가 처리상태가 처리완료일 때 버튼 활성화 그렇지 않을경우 버튼미활성

--- src/main/java/beeNb/dao/ComplainDAO.java
+++ src/main/java/beeNb/dao/ComplainDAO.java
@@ -4,39 +4,71 @@ import java.util.*;
 
 public class ComplainDAO {
 	// 설명 : complainlist 보여주기
-		// 호출 : customerComplainList.jsp
-		// return : ArrayList<HashMap<String,Object>>
-		public static ArrayList<HashMap<String,Object>> selectComplainList(String customerId) throws Exception {
-			ArrayList<HashMap<String,Object>> list = new ArrayList<HashMap<String,Object>>();
-			Connection conn = DBHelper.getConnection();
-			String sql = "SELECT  b.booking_no bookingNo, c.complain_no complainNo, c.complain_type complainType, "
-					+ "c.complain_content complainContent, c.complain_state complainState, c.complain_answer complainAnswer, "
-					+ "c.create_date createDate, c.update_date updateDate "
-					+ "FROM complain c INNER JOIN booking b "
-					+ "ON c.booking_no = b.booking_no "
-					+ "WHERE b.customer_id = ? "
-					+ "ORDER BY b.create_date";
-			PreparedStatement stmt = conn.prepareStatement(sql);
-			stmt.setString(1,customerId);
-			// 디버깅코드
-			System.out.println("stmt:"+stmt);
-			
-			ResultSet rs = stmt.executeQuery();
-			
-			while(rs.next()){
-				HashMap<String,Object> m = new HashMap<String,Object>();
-				m.put("bookingNo", rs.getInt("bookingNo"));
-				m.put("complainNo", rs.getInt("complainNo"));
-				m.put("complainType", rs.getString("complainType"));
-				m.put("complainContent", rs.getString("complainContent"));
-				m.put("complainState", rs.getString("complainState"));
-				m.put("complainAnswer", rs.getString("complainAnswer"));
-				m.put("createDate", rs.getString("createDate"));
-				m.put("updateDate", rs.getString("updateDate"));
-				list.add(m);
-			}
-			
-			conn.close();
-			return list;
+	// 호출 : customerComplainList.jsp
+	// return : ArrayList<HashMap<String,Object>>
+	public static ArrayList<HashMap<String,Object>> selectComplainList(String customerId) throws Exception {
+		ArrayList<HashMap<String,Object>> list = new ArrayList<HashMap<String,Object>>();
+		Connection conn = DBHelper.getConnection();
+		String sql = "SELECT  b.booking_no bookingNo, c.complain_no complainNo, c.complain_type complainType, "
+				+ "c.complain_content complainContent, c.complain_state complainState, c.complain_answer complainAnswer, "
+				+ "c.create_date createDate, c.update_date updateDate "
+				+ "FROM complain c INNER JOIN booking b "
+				+ "ON c.booking_no = b.booking_no "
+				+ "WHERE b.customer_id = ? "
+				+ "ORDER BY b.create_date";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setString(1,customerId);
+		// 디버깅코드
+		System.out.println("stmt:"+stmt);
+		
+		ResultSet rs = stmt.executeQuery();
+		
+		while(rs.next()){
+			HashMap<String,Object> m = new HashMap<String,Object>();
+			m.put("bookingNo", rs.getInt("bookingNo"));
+			m.put("complainNo", rs.getInt("complainNo"));
+			m.put("complainType", rs.getString("complainType"));
+			m.put("complainContent", rs.getString("complainContent"));
+			m.put("complainState", rs.getString("complainState"));
+			m.put("complainAnswer", rs.getString("complainAnswer"));
+			m.put("createDate", rs.getString("createDate"));
+			m.put("updateDate", rs.getString("updateDate"));
+			list.add(m);
 		}
+		
+		conn.close();
+		return list;
+	}
+	// 설명 : complain 상세보기 
+	// 호출 : customerComplainOne.jsp
+	// return : <HashMap<String,Object>>
+	public static HashMap<String, Object> selectComplainOne(String complainNo) throws Exception {
+		HashMap<String, Object> map = null;
+		Connection conn = DBHelper.getConnection();
+		String sql = "SELECT  b.booking_no bookingNo, c.complain_no complainNo, c.complain_type complainType, "
+				+ "c.complain_content complainContent, c.complain_state complainState, c.complain_answer complainAnswer, "
+				+ "c.create_date createDate, c.update_date updateDate FROM complain c INNER JOIN booking b "
+				+ "ON c.booking_no = b.booking_no "
+				+ "WHERE c.complain_no = ? ";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setString(1,complainNo );
+		// 디버깅코드
+		System.out.println("stmt:"+stmt);
+		ResultSet rs = stmt.executeQuery();
+		while(rs.next()) {
+			map = new HashMap<String, Object>();
+			map.put("complainNo", rs.getInt("complainNo"));
+			map.put("bookingNo", rs.getInt("bookingNo"));
+			map.put("complainType", rs.getString("complainType"));
+			map.put("complainState", rs.getString("complainState"));
+			map.put("complainAnswer", rs.getString("complainAnswer"));
+			map.put("createDate", rs.getString("createDate"));
+			map.put("updateDate", rs.getString("updateDate"));
+			
+		}
+		
+		conn.close();
+		return map;
+	}
+	
 }
