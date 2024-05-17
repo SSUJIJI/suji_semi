c30c99d07901d919986a4afc81e41dade9e61c7f
hyred-tty0806 <hyukhee114@gmail.com>
2024-05-17 16:32:35 +0900

    [정혁희] 2024-05-17 commit3 [추가] 1. 민원 리스트, 상세보기, 민원 접수처리 작업 완료

--- src/main/java/beeNb/dao/ComplainDAO.java
+++ src/main/java/beeNb/dao/ComplainDAO.java
@@ -39,36 +39,169 @@ public class ComplainDAO {
 		conn.close();
 		return list;
 	}
+	
+	
 	// 설명 : complain 상세보기 
 	// 호출 : customerComplainOne.jsp
-	// return : <HashMap<String,Object>>
-	public static HashMap<String, Object> selectComplainOne(String complainNo) throws Exception {
-		HashMap<String, Object> map = null;
+	// return : <HashMap<String,String>>
+	public static HashMap<String, String> selectComplainOne(String complainNo) throws Exception {
+		HashMap<String, String> map = new HashMap<String, String>();
 		Connection conn = DBHelper.getConnection();
-		String sql = "SELECT  b.booking_no bookingNo, c.complain_no complainNo, c.complain_type complainType, "
+		String sql = "SELECT  c.complain_no complainNo, c.complain_type complainType,"
 				+ "c.complain_content complainContent, c.complain_state complainState, c.complain_answer complainAnswer, "
-				+ "c.create_date createDate, c.update_date updateDate FROM complain c INNER JOIN booking b "
-				+ "ON c.booking_no = b.booking_no "
+				+ "c.create_date createDate, c.update_date updateDate FROM complain c "
 				+ "WHERE c.complain_no = ? ";
 		PreparedStatement stmt = conn.prepareStatement(sql);
-		stmt.setString(1,complainNo );
+		stmt.setString(1,complainNo);
 		// 디버깅코드
 		System.out.println("stmt:"+stmt);
 		ResultSet rs = stmt.executeQuery();
 		while(rs.next()) {
-			map = new HashMap<String, Object>();
-			map.put("complainNo", rs.getInt("complainNo"));
-			map.put("bookingNo", rs.getInt("bookingNo"));
+			map.put("complainNo", rs.getString("complainNo"));
 			map.put("complainType", rs.getString("complainType"));
+			map.put("complainContent", rs.getString("complainContent"));
 			map.put("complainState", rs.getString("complainState"));
 			map.put("complainAnswer", rs.getString("complainAnswer"));
 			map.put("createDate", rs.getString("createDate"));
 			map.put("updateDate", rs.getString("updateDate"));
-			
 		}
-		
 		conn.close();
 		return map;
 	}
+	
+	
+	
+	
+	// 설명 : complainList 개수
+	// 호출 : empComplainList.jsp
+	// return : ArrayList<HashMap<String,Object>>
+	public static int selectEmpComplainListCnt(String complainState) throws Exception{
+		int cnt = 0;
+		
+		Connection conn = DBHelper.getConnection();
+		String sql = "SELECT "
+				+ "count(*) cnt "
+				+ "FROM complain c "
+				+ "WHERE 1=1 ";
+		
+		// complainState에 따른 분기 발생으로 인해 stmt 먼저 생성
+		PreparedStatement stmt = null;
+		
+		// 예약리스트 전체 조회시
+		if(complainState.equals("all")) {
+			stmt = conn.prepareStatement(sql);
+		} else {
+			// complainState이 있는 경우(select박스로 숙소별로 예약 리스트 조회시)
+			sql = sql + " AND c.complain_state = ? ";
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, complainState);
+		}
+		
+		ResultSet rs = stmt.executeQuery();
+		
+		if(rs.next()) {
+			cnt = rs.getInt("cnt");
+		}
+		
+		conn.close();
+		return cnt;
+	}
+	
+	
+	// 설명 : vocList 조회
+	// 호출 : vocList.jsp
+	// return : ArrayList<HashMap<String, Object>>
+	public static ArrayList<HashMap<String, Object>> selectVOCList(String complainState, int startRow, int rowPerPage) throws Exception{
+		ArrayList<HashMap<String, Object>> vocList = new ArrayList<>();
+		
+		Connection conn = DBHelper.getConnection();
+		
+		
+		String sql = "SELECT "
+				+ "c.complain_no complainNo, "
+				+ "c.complain_type complainType, "
+				+ "c.complain_content complainContent, "
+				+ "c.complain_state complainState, "
+				+ "c.complain_answer complainAnswer, "
+				+ "c.create_date createDate, "
+				+ "c.update_date updateDate "
+				+ "FROM complain c "
+				+ "WHERE 1=1 ";
+		
+		// complain_state에 따른 분기 발생으로 인해 stmt 먼저 생성
+		PreparedStatement stmt = null;
+		
+		if(complainState.equals("all")) {
+			sql = sql + " ORDER BY c.create_date DESC LIMIT ?,?";
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, startRow);
+			stmt.setInt(2, rowPerPage);
+		} else {
+			// complain_state이 있는 경우
+			sql = sql + " AND c.complain_state = ?"
+					+ " ORDER BY c.create_date DESC"
+					+ "	LIMIT ?,?";
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, complainState);
+			stmt.setInt(2, startRow);
+			stmt.setInt(3, rowPerPage);
+			
+		}
+		
+		
+		ResultSet rs = stmt.executeQuery();
+
+		while(rs.next()) {
+			HashMap<String, Object> m = new HashMap<>();
+			m.put("complainNo", rs.getInt("complainNo"));
+			m.put("complainType", rs.getString("complainType"));
+			m.put("complainContent", rs.getString("complainContent"));
+			m.put("complainState", rs.getString("complainState"));
+			m.put("complainAnswer", rs.getString("complainAnswer"));
+			m.put("createDate", rs.getString("createDate"));
+			m.put("updateDate", rs.getString("updateDate"));
+			vocList.add(m);
+		}
+		
+		conn.close();
+		return vocList;
+	}
+	
+	
+	// 설명 : 민원 상태 업데이트
+	// 호출 : /BeeNb/emp/updateVocState.jsp
+	// return : int (업데이트 성공시 1)
+	public static int updateVoc(int complainNo, String complainState, String complainAnswer) throws Exception {
+		System.out.println("11");
+		int row = 0;
+		Connection conn = DBHelper.getConnection();
+		
+		String sql = "";
+		PreparedStatement stmt = null;
+		if(complainState.equals("처리중")) {
+			sql = "UPDATE complain SET complain_state = ? WHERE complain_no = ?";
+			System.out.println("11");
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, complainState);
+			stmt.setInt(2, complainNo);
+		}else if(complainState.equals("처리완료")){
+			System.out.println("22");
+			sql = "UPDATE complain SET complain_state = ?, complain_answer = ? WHERE complain_no = ?";
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, complainState);
+			stmt.setString(2, complainAnswer);
+			stmt.setInt(3, complainNo);
+		}
+		// 쿼리 디버깅
+		System.out.println("stmt : " + stmt);
+		row = stmt.executeUpdate();
+		//자원반납
+		conn.close();		
+		return row;
+	}
+	
+
+	
+	
 	
 }
